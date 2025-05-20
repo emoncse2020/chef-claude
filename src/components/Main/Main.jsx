624969b1a53229e132cc1f3e4d90caeb337fc65a
emoncse2020 <emon.cse.2020@gmail.com>
2025-05-20 22:47:23 +0600

    form handing done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import "./Main.css";
 
 const Main = () => {
+  const [ingredients, setIngredient] = useState([]);
+
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient");
+    setIngredient((prevIngredient) => [...prevIngredient, newIngredient]);
+  }
   return (
     <main>
-      <form className="add-ingredient-form">
-        <input aria-label="Add ingredient" placeholder="e.g oregano" />
+      <form action={addIngredient} className="add-ingredient-form">
+        <input
+          aria-label="Add ingredient"
+          placeholder="e.g oregano"
+          name="ingredient"
+        />
         <button>Add ingredient</button>
       </form>
+
+      <ul>{ingredientsListItems}</ul>
     </main>
   );
 };
